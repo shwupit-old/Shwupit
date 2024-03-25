4dc5ecaed64f163306acec57bcd51f62f4fe8725
DemiAina <100835583+DemiAina@users.noreply.github.com>
2024-03-25 16:21:26 +0000

    Docker compose file had been made to make excutions of the database easy, go starter code to connect to the database and go dependency have been added

---
+++ backend/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gocql/gocql"
+	"log"
+)
+
+func main() {
+
+	cluster := gocql.NewCluster("127.0.0.1:9042")
+	// cluster.Keyspace = "user_data"
+	cluster.Consistency = gocql.All
+
+	session, err := cluster.CreateSession()
+	if err != nil {
+		log.Fatalf("Could not connect to Cassandra: %v", err)
+	}
+	defer session.Close()
+
+    cql := "CREATE KEYSPACE IF NOT EXISTS my_keyspace WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3'} AND durable_writes = true;"
+
+	if err := session.Query(cql).Exec(); err != nil {
+		log.Fatalf("Failed to create keyspace: %v", err)
+	} else {
+		fmt.Println("Keyspace created successfully")
+	}
+}
