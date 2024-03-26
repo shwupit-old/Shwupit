22192212f9d9710fa8c0b4f84574f1d7fd3739e1
DemiAina <100835583+DemiAina@users.noreply.github.com>
2024-03-26 13:17:39 +0000

    Cleanup: compose.yml, Updates: go.mod, go.sum -> dependencies , added: db package -> tests connection

--- backend/main.go
+++ backend/main.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"github.com/gocql/gocql"
 	"log"
+	"api/db"
 )
 
 func main() {
-
+    
+    db.TestConnection()
 	cluster := gocql.NewCluster("127.0.0.1:9042")
 	// cluster.Keyspace = "user_data"
 	cluster.Consistency = gocql.All
@@ -18,7 +20,7 @@ func main() {
 	}
 	defer session.Close()
 
-    cql := "CREATE KEYSPACE IF NOT EXISTS my_keyspace WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3'} AND durable_writes = true;"
+    cql := "CREATE KEYSPACE IF NOT EXISTS user_data WITH replication = {'class': 'SimpleStrategy', 'replication_factor': '3'} AND durable_writes = true;"
 
 	if err := session.Query(cql).Exec(); err != nil {
 		log.Fatalf("Failed to create keyspace: %v", err)
