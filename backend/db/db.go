39a9b274aefd7a297f9affea8e7ecf2d6f52ac22
DemiAina <100835583+DemiAina@users.noreply.github.com>
2024-03-27 15:44:57 +0000

    Added in login in functionaity, db.go has a new function for looking for users,Note password will be added later

--- backend/db/db.go
+++ backend/db/db.go
@@ -3,7 +3,6 @@ package db
 import (
 	"api/model"
 	"log"
-
 	"github.com/gocql/gocql"
 )
 
@@ -28,4 +27,28 @@ func InsertSwapper(swapper model.Swapper) error {
 		log.Printf("Failed to insert swapper: %+v, error: %v\n", swapper, err) // Log the error
 	}
 	return err
+}
+
+
+func GetUserByUsername(username string) (*model.Swapper, error) {
+    var swapper model.Swapper
+
+	err := session.Query(`SELECT user_id, username, email, location, swapping_history, availability, profile_picture, account_creation_date FROM swappers WHERE username = ? LIMIT 1 ALLOW FILTERING`, username).Consistency(gocql.One).Scan(
+		&swapper.UserID,
+		&swapper.Username,
+		&swapper.Email,
+		&swapper.Location,
+		&swapper.SwappingHistory,
+		&swapper.Availability,
+		&swapper.ProfilePicture,
+		&swapper.AccountCreationDate,
+	)
+	
+    
+    if err != nil {
+        log.Printf("Failed to retrieve swapper by username '%s': %v", username, err)
+        return nil, err
+    }
+	log.Printf("Login successful welcome '%s'" ,username)
+    return &swapper, nil
 }
